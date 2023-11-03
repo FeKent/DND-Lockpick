0c49e84d4173097e0dc8afec559fb227e9cbf815
SnippyRex <kentf022@gmail.com>
2023-11-03 20:02:44 +0000

    Add Compose Navigation & link initial screens

--- app/src/main/java/com/example/dndlockpick/MainActivity.kt
+++ app/src/main/java/com/example/dndlockpick/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dndlockpick
 
+import LandingScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.dndlockpick.model.TumblerScreen
 import com.example.dndlockpick.ui.theme.DNDLockpickTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +28,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DNDLockpick()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+sealed class Screen(val route: String) {
+    object Landing : Screen("Landing")
+
+    object Tumbler : Screen("Tumbler")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DNDLockpickTheme {
-        Greeting("Android")
+@androidx.compose.runtime.Composable
+fun DNDLockpick() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Landing.route){
+        composable(Screen.Landing.route){ LandingScreen() }
+        composable(Screen.Tumbler.route){ TumblerScreen() }
     }
+
 }
---
+++ app/src/main/java/com/example/dndlockpick/model/TumblerScreen.kt
@@ -0,0 +1,9 @@
+package com.example.dndlockpick.model
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun TumblerScreen() {
+
+}
