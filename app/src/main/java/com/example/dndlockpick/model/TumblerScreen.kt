045ae67598bce2a555f74436ab7ed55707a90d0e
SnippyRex <kentf022@gmail.com>
2023-11-08 00:29:21 +0000

    Adds "shake" animation inside the box if clicked out of sequence

--- app/src/main/java/com/example/dndlockpick/model/TumblerScreen.kt
+++ app/src/main/java/com/example/dndlockpick/model/TumblerScreen.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -30,7 +32,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,11 +44,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.dndlockpick.viewmodel.LockpickViewModel
+import kotlin.math.roundToInt
 
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -62,7 +69,20 @@ fun TumblerScreen(
     val context = LocalContext.current
     val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
 
-    val shake = remember { Animatable(0f) }
+    val shake = remember { Animatable(0f)}
+    var trigger by remember { mutableStateOf(0L) }
+
+    LaunchedEffect(trigger){
+        if (trigger != 0L){
+            for (i in 0..10) {
+                when (i % 2) {
+                    0 -> shake.animateTo(5f, spring(stiffness = 100_000f))
+                    else -> shake.animateTo(-5f, spring(stiffness = 100_000f))
+                }
+            }
+            shake.animateTo(0f)
+        }
+    }
 
     Column {
         TumblerScreenBar { backHome() }
@@ -83,6 +103,8 @@ fun TumblerScreen(
                             .clip(RoundedCornerShape(4.dp))
                             .border(width = 2.dp, color = Color.Black)
                             .background(backgroundColor)
+                            .offset { IntOffset(shake.value.roundToInt(), y = 0) }
+                            .padding(horizontal = 24.dp, vertical = 8.dp)
                             .clickable {
                                 if (!isSelected) {
                                     // Check if the selected tumbler is the correct one
@@ -95,6 +117,7 @@ fun TumblerScreen(
                                                 VibrationEffect.DEFAULT_AMPLITUDE
                                             )
                                             vibrator.vibrate(vibrationEffect)
+                                            trigger = System.currentTimeMillis()
                                         }
                                     }
                                     lockpickViewModel.toggleTumblerSelection(
