f5d831273421aadcfabdaa27e0c9fed9af0342e0
FeKent <kentf022@gmail.com>
2024-03-15 15:17:55 +0000

    Start scaffolding for the navigation drawer

--- app/src/main/java/com/fekent/dndlockpick/composables/LandingScreen.kt
+++ app/src/main/java/com/fekent/dndlockpick/composables/LandingScreen.kt
@@ -18,16 +18,20 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -44,6 +48,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fekent.dndlockpick.R
 import com.fekent.dndlockpick.viewmodel.LandingViewModel
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
 
