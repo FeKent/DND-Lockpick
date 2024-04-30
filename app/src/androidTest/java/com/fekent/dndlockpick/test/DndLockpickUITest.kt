f07b0aeaf04d1e81f5a536487128c74d5c7bd47d
FeKent <kentf022@gmail.com>
2024-04-30 01:09:23 +0100

    Add a basic unit test

--- app/src/androidTest/java/com/fekent/dndlockpick/test/DndLockpickUITest.kt
+++ app/src/androidTest/java/com/fekent/dndlockpick/test/DndLockpickUITest.kt
@@ -1,8 +1,10 @@
 package com.fekent.dndlockpick.test
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import com.fekent.dndlockpick.composables.CustomScreen
+import com.fekent.dndlockpick.composables.CustomScreenBar
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,6 +22,15 @@ class DndLockpickUITest {
 
         composeTestRule.onNodeWithText("Number of Tumblers:").assertExists()
 
+    }
+
+    @Test
+    fun customAppBarTest(){
+        composeTestRule.setContent {
+            CustomScreenBar{}
+        }
+
+        composeTestRule.onNodeWithText("custom mode", ignoreCase = true).assertIsDisplayed()
     }
 
 }
