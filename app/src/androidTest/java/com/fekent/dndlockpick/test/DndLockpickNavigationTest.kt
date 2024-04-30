0af0dfc3eba3dc84a398f8d05edd098c0c2d5ad2
FeKent <kentf022@gmail.com>
2024-04-30 12:04:05 +0100

    Add navigation test using helper methods

--- app/src/androidTest/java/com/fekent/dndlockpick/test/DndLockpickNavigationTest.kt
+++ app/src/androidTest/java/com/fekent/dndlockpick/test/DndLockpickNavigationTest.kt
@@ -3,6 +3,9 @@ package com.fekent.dndlockpick.test
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.NavController
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.fekent.dndlockpick.DNDLockpick
@@ -30,5 +33,16 @@ class DndLockpickNavigationTest {
     @Test
     fun dndLockpickNavHost_verifyStartDestination(){
         assertEquals(Screen.Mode.route,navController.currentDestination?.route)
+    }
+
+    fun NavController.assertCurrentRouteName(expectedRouteName: String){
+        assertEquals(expectedRouteName, currentDestination?.route)
+    }
+
+    @Test
+    fun navController_clickCustom_navigatesToCustomScreen(){
+        composeTestRule.onNodeWithText("custom game", ignoreCase = true)
+            .performClick()
+        navController.assertCurrentRouteName(Screen.Custom.route)
     }
 }
