d13d1c24df276f86a5a248d1e68e985bfe1ce41a
FeKent <kentf022@gmail.com>
2026-03-01 00:12:18 +0000

    Update Version for Google Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fekent.dndlockpick"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
